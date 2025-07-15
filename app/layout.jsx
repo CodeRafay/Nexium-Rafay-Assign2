92f83b051c632be0dab9bdb3ac1468c11327584d
CodeRafay <f223327@cfd.nu.edu.pk>
2025-07-15 18:25:57 +0400

    fix : favicon.ico

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,8 +11,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="./favicon.ico" type="image/x-icon" />
+        <meta charSet="UTF-8" />
+      </head>
       <body className={`${inter.className} bg-background text-foreground min-h-screen`}>
-        <main className="container mx-auto p-4 max-w-full overflow-hidden">{children}</main>
+        <main className="container mx-auto p-4 max-w-full overflow-hidden">
+          {children}
+        </main>
       </body>
     </html>
   );
